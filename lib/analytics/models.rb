d75e162b803489d179b153855423b26857565d8c
harikrishnan <harikrishnan.a@infibeam.net>
2015-03-27 15:58:27 +0530

    load all ruby files as dependency for models

--- lib/analytics/models.rb
+++ lib/analytics/models.rb
@@ -1,11 +1,8 @@
 module Analytics
   module Models
-    require_dependency 'analytics/models/base'
-    require_dependency 'analytics/models/tokens'
-
-    require_dependency 'analytics/models/google_analytics'
-    require_dependency 'analytics/models/classic_google_analytics'
-    require_dependency 'analytics/models/universal_google_analytics'
+    Dir.glob(File.dirname(__FILE__)+'/analytical/models/*.rb').each do |file|
+      require_dependency file
+    end
   end
 end
 
